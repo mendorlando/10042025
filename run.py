8d0b11117a171094fa961e636525a64c8a29ba63
Orlando <orliinuevagen@gmail.com>
2025-04-07 09:07:17 -0600

    Initial commit

---
+++ run.py
@@ -0,0 +1,8 @@
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
