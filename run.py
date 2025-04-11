b42f21cc96dae4cefcd274a2ff181d5cea6d140a
Orlando <orliinuevagen@gmail.com>
2025-04-10 21:30:07 -0600

    Corrigiendo código en run.py para despliegue en Render

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
+from os import environ
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    from os import environ
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(environ.get("PORT", 5000)))
